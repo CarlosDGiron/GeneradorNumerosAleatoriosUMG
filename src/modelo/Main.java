cc69972d3c996989ba03779a71451de77bb7e247
CarlosDGiron <carlosdavidgironleonardo@gmail.com>
2024-02-17 19:00:13 -0600

    Finalizado

---
+++ src/modelo/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modelo;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import vista.Form;
+
+/**
+ *
+ * @author cana0
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+       Form f=new Form();
+       f.show();
+    }
+}
+
+
